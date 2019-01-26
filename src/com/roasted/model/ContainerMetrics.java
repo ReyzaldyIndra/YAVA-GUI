7926bee4bec17e50960e59f0ec3bef83a20febe9
Reyzaldy Indra <ireyzaldi@gmail.com>
2019-01-27 06:24:00 +0700

    updated data class

--- src/com/roasted/model/ContainerMetrics.java
+++ src/com/roasted/model/ContainerMetrics.java
@@ -2,67 +2,83 @@ package com.roasted.model;
 
 import java.io.Serializable;
 	
-	@SuppressWarnings("serial")
+	
 	public  class ContainerMetrics implements Serializable{
 	
 //	static final long serialVersionUID = 4553230673859196378L;
-	public long DURATION;
-	public String APP_ID, CONTAINER_ID, HOSTNAME;
-	public java.util.Date START_TIME, FINISH_TIME;
+	public long duration;
+	public String app_id, container_id, hostname;
+	
+	public long current_time;
+	public long start_time, finish_time;
+	
+	public ContainerMetrics(String app_id, String container_id, String hostname, long duration, long current_time, long start_time, long finish_time) {
+		// TODO Auto-generated constructor stub
+		this.app_id =  app_id;
+		this.container_id = container_id;
+		this.hostname = hostname;
+		this.duration = duration;
+		this.current_time = current_time;
+		this.start_time = start_time;
+		this.finish_time = finish_time;
+		
+	}
 	
 	public ContainerMetrics() {
 		// TODO Auto-generated constructor stub
 	}
-	public ContainerMetrics(String APP_ID, String CONTAINER_ID, String HOSTNAME, long DURATION, java.util.Date START_TIME, java.util.Date FINISH_TIME) {
-		// TODO Auto-generated constructor stub
-		this.APP_ID =  APP_ID;
-		this.CONTAINER_ID = CONTAINER_ID;
-		this.HOSTNAME = HOSTNAME;
-		this.DURATION = DURATION;
-		this.START_TIME = START_TIME;
-		this.FINISH_TIME = FINISH_TIME;
-	}
-	
+
 	public String getAppId() {
-		return APP_ID;
+		return app_id;
 	}
-	public void setAppId(String APP_ID){
-		this.APP_ID = APP_ID;
+	public void setAppId(String app_id){
+		this.app_id = app_id;
 	}
 	
 	public String getContainerId(){
-		return CONTAINER_ID;
+		return container_id;
 	}
 
-	public void setContainerId(String CONTAINER_ID){
-		this.CONTAINER_ID=CONTAINER_ID;
+	public void setContainerId(String container_id){
+		this.container_id=container_id;
 	}
 	
 	public String getHostname() {
-		return APP_ID;
+		return app_id;
 	}
-	public void setHostname(String HOSTNAME){
-		this.HOSTNAME = HOSTNAME;
-	}
-	
-	public java.util.Date getStartTime() {
-		return START_TIME;
-	}
-	public void setStartTime(java.util.Date START_TIME){
-		this.START_TIME = START_TIME;
+	public void setHostname(String hostname){
+		this.hostname = hostname;
 	}
 	
-	public java.util.Date getFinishTime() {
-		return START_TIME;
+	public long getCurrentTime() {
+		return current_time;
 	}
-	public void setFinishTime(java.util.Date FINISH_TIME){
-		this.FINISH_TIME = FINISH_TIME;
+	public void setCurrentTime(long current_time) {
+		this.current_time = current_time;
 	}
 	
 	public long getDuration() {
-		return DURATION;
+		return duration;
 	}
-	public void setDuration(long DURATION){
-		this.DURATION = DURATION;
+	public void setDuration(long duration){
+		this.duration = duration;
 	}
+
+	public long getStartTime() {
+		return start_time;
+	}
+
+	public void setStartTime(long start_time) {
+		this.start_time = start_time;
+	}
+
+	public long getFinishTime() {
+		return finish_time;
+	}
+
+	public void setFinishTime(long finish_time) {
+		this.finish_time = finish_time;
+	}
+
+	
 }
