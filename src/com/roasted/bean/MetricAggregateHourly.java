32e9c747d280aae808db84283fdd7e1ccda52457
Reyzaldy Indra <ireyzaldi@gmail.com>
2019-01-27 06:23:09 +0700

    other tables from ambari-metrics-collector

--- src/com/roasted/bean/MetricAggregateHourly.java
+++ src/com/roasted/bean/MetricAggregateHourly.java
@@ -5,15 +5,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.util.Properties;
 
-import com.roasted.model.ContainerMetrics;
-
-import java.util.List;
-import java.util.ArrayList;
-
-public class MetricsAggregateDaily {
-	
-	
-	
+public class MetricAggregateHourly {
 	
 	public static void main(String args[]) throws Exception {
 	    Connection conn;
@@ -24,13 +16,10 @@ public class MetricsAggregateDaily {
 	    conn =  DriverManager.getConnection("jdbc:phoenix:192.168.3.132:61181:/ams-hbase-unsecure");
 
 	    System.out.println("got connection");
-
 	    
-	    ResultSet rst = conn.createStatement().executeQuery("select * from METRICS_AGGREGATE_DAILY");
-//	    
+	    ResultSet rst = conn.createStatement().executeQuery("select * from METRIC_AGGREGATE_HOURLY_UUID");
 	    while (rst.next()) {
-	      
-	      
+	      System.out.println(rst.getString(1) + " " + rst.getString(2) + " " + rst.getString(3) + " " + rst.getString(4) + " " + rst.getString(5) + " " + rst.getString(6));
 	    }
 	  }
 
