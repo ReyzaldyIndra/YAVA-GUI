9fa0616944f89b95231c885a1e6481290f68413a
Reyzaldy Indra <ireyzaldi@gmail.com>
2019-01-27 06:25:44 +0700

    temporary view config

--- src/com/roasted/view/ContainerMetricsView.java
+++ src/com/roasted/view/ContainerMetricsView.java
@@ -6,38 +6,58 @@ import javax.annotation.PostConstruct;
 //import javax.faces.bean.ManagedBean;
 //import javax.faces.bean.ManagedProperty;
 //import javax.faces.bean.ViewScoped;
-import javax.inject.Inject;
-import javax.inject.Named;
-import java.sql.SQLException;
+//import javax.inject.Inject;
+//import javax.inject.Named;
 
+
+//import java.sql.SQLException;
+import javax.enterprise.context.ApplicationScoped;
+//import javax.faces.bean.ManagedProperty;
+import javax.inject.Named;
+//import javax.inject.Inject;
+//import java.io.Serializable;
 import com.roasted.model.ContainerMetrics;
 import com.roasted.network.ContainerMetricsConnect;
 
-
-//import javax.enterprise.context.SessionScoped;
-
-import javax.enterprise.context.ApplicationScoped;
-
-@Named("ContainerMetrics")
+@Named("cmview")
 @ApplicationScoped
+
 public class ContainerMetricsView {
-	private List<ContainerMetrics> _container_metrics;
+
+	public List<ContainerMetrics> getContainermetrics() {
+		return containermetrics;
+	}
+
+	public void setContainermetrics(List<ContainerMetrics> containermetrics) {
+		this.containermetrics = containermetrics;
+	}
+
+	public ContainerMetricsConnect getConnect() {
+		return connect;
+	}
+
+	private List<ContainerMetrics> containermetrics;
 	
-	
+//	@ManagedProperty("#{ContainerMetricsConnect}")
 	private ContainerMetricsConnect connect;
 	
 	@PostConstruct
-	public void init() throws SQLException, Exception {
-//		_container_metrics = connect.selectTable();
+	public void init(){
+		try {
+			containermetrics = connect.containerMetrics();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
-	
 	
 	public List<ContainerMetrics> getContainerMetrics(){
-		return _container_metrics;
+		return containermetrics;
 	}
-	
-//	public void setConnect(ContainerMetricsConnect connect) {
-//		this.connect = connect;
-//	}
+
+	public void setConnect(ContainerMetricsConnect connect) {
+		this.connect = connect;
+	}
 
 }
