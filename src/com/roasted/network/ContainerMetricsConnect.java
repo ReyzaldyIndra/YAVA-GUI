1e5f1ad98009c73f9883f47dbb9905773eebca20
Reyzaldy Indra <ireyzaldi@gmail.com>
2019-01-27 06:24:48 +0700

    temp jdbc config

--- src/com/roasted/network/ContainerMetricsConnect.java
+++ src/com/roasted/network/ContainerMetricsConnect.java
@@ -1,34 +1,21 @@
 package com.roasted.network;
 
-
-
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-//import java.sql.Statement;
-//import java.util.HashSet;
-//import java.util.Set;
-//import java.sql.PreparedStatement;
-//import java.sql.Connection;
+import java.util.Properties;
+import java.util.Date;
 
 import javax.enterprise.context.ApplicationScoped;
+//import javax.inject.Named;
+import javax.faces.bean.ManagedBean;
 
-import javax.inject.Named;
 
-import java.io.IOException;
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.hbase.Cell;
-import org.apache.hadoop.hbase.CellUtil;
-import org.apache.hadoop.hbase.HBaseConfiguration;
-import org.apache.hadoop.hbase.TableName;
-import org.apache.hadoop.hbase.client.Connection;
-import org.apache.hadoop.hbase.client.ConnectionFactory;
-import org.apache.hadoop.hbase.client.HTable;
-import org.apache.hadoop.hbase.client.Result;
-import org.apache.hadoop.hbase.client.ResultScanner;
-import org.apache.hadoop.hbase.client.Scan;
-import org.apache.hadoop.hbase.client.Table;
-import org.apache.hadoop.hbase.util.Bytes;
+import com.roasted.model.ContainerMetrics;
+
+import java.util.List;
+import java.util.ArrayList;
 
 
 
@@ -36,71 +23,46 @@ import org.apache.hadoop.hbase.util.Bytes;
 //untuk setup koneksi dengan query Hbase
 
 //@SuppressWarnings("serial")
-@Named("metrics")
+@ManagedBean(name="ContainerMetricsConnect")
 @ApplicationScoped
 public class ContainerMetricsConnect{
-	@SuppressWarnings("deprecation")
-	public static void main(String args[]) throws IOException {
-		
-		// Instantiating Configuration class
-		
-		Configuration conf = HBaseConfiguration.create();
-		
-		System.out.println("got conf  ...");
-		
-		conf.addResource(new Path("/home/reyzaldy/git/YAVA-GUI/maven-project/hbase-site.xml"));
-		conf.addResource(new Path("/home/reyzaldy/git/YAVA-GUI/maven-project/ams-site.xml"));
-		
-		Path p = new Path("/home/reyzaldy/git/YAVA-GUI/maven-project/hbase-site.xml");
-		
-		System.out.println("got "+ p.toString());		
-		
-		System.out.println("got conf  ... 2");		
-		
-		Connection connection = ConnectionFactory.createConnection(conf);
-		
-		 System.out.println("got conn ...");
-		 		
-		// Instantiating HTable class
-		//HTable table = new HTable(config, "emp");
-		
-		Table table = connection.getTable(TableName.valueOf("CONTAINER_METRICS"));
-		
-		System.out.println("got table  ... ");
-		
-		
-		// Instantiating the Scan class
-		Scan scan = new Scan();
-//		scan.addColumn("B@5119fb47", qualifier)
-		// Scanning the required columns
-	    ResultScanner scanner = table.getScanner(scan);
-		
-		// Getting the scan result
-		// Reading values from scan result
-		for (Result result = scanner.next(); result != null; result = scanner.next()) {
-			
-			System.out.println("Found row : " + result);
-			String app_id =  Bytes.toString(result.getRow());
-			System.out.println("Row : " + app_id);
-//			System.out.println("Row : " + a);
-//			System.out.println("result :" + result.getMap());
-			for(Cell cell : result.listCells()) {
-//				System.out.println(cell.getType());
-//				System.out.println(cell);
-//				System.out.println(cell.getValueArray().toString());
-				String family = Bytes.toString(CellUtil.cloneFamily(cell));
-				String qualifier = Bytes.toString(CellUtil.cloneQualifier(cell));
+	
+	
+		 public List<ContainerMetrics> containerMetrics() throws ClassNotFoundException, SQLException{
 				
-				System.out.println("Family : " + family);
-				System.out.println("Qualifier : " + qualifier);
+				List<ContainerMetrics> container_metrics = new ArrayList<ContainerMetrics>();
 				
-				long timeStamp = cell.getTimestamp();
-				System.out.println("Timestamp: " + timeStamp);
+				Connection conn;
+			    Properties prop = new Properties();
+			    
+			    Class.forName("org.apache.phoenix.jdbc.PhoenixDriver");
+			    conn =  DriverManager.getConnection("jdbc:phoenix:192.168.3.132:61181:/ams-hbase-unsecure");
+			    
+			    ResultSet rst;
+			    rst = conn.createStatement().executeQuery("select * from CONTAINER_METRICS");
+			    
+		    	ContainerMetrics cm = new ContainerMetrics();
+			    while(rst.next()) {
+			    	
+			    	cm.setAppId(rst.getString("app_id"));
+			    	cm.setContainerId(rst.getString("container_id"));
+			    	cm.setHostname(rst.getString("hostname"));
+			    	cm.setDuration(rst.getLong("duration"));
+			    	cm.setCurrentTime(System.currentTimeMillis());
+			    	Date st = rst.getDate("start_time");
+			    	cm.setStartTime(st.getTime());
+			    	Date ft = rst.getDate("finish_time");
+			    	cm.setFinishTime(ft.getTime());
+
+			    	
+			    	container_metrics.add(cm);
+			    	System.out.println(cm);
+			    	
+			    	
+			    }
+				
+				
+				return container_metrics;
+				
 			}
-			
-			
-		// closing the scanner
 		}
-		scanner.close();
-	}
-}
